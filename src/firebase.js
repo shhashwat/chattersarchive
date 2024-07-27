b1cfce7e0f9a6839725b6988ce23b7aff848fb79
Shashwat <sshashwatssingh@gmail.com>
2024-07-28 02:23:55 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFWrbFr9apjastknNMgdEV2JlBmgEm20c",
+  authDomain: "bolbokenae.firebaseapp.com",
+  projectId: "bolbokenae",
+  storageBucket: "bolbokenae.appspot.com",
+  messagingSenderId: "159143368623",
+  appId: "1:159143368623:web:b538616b3eae050ef2015a"
+};
+
+export const app = initializeApp(firebaseConfig);
